6abecea31ecdcc233dd3bd213cc8ca4711558095
donaramyde <aramyde777@gmail.com>
2022-08-26 20:01:15 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/plearncode/mynotess/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.plearncode.mynotess
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/login_view.dart
@@ -0,0 +1,96 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotess/firebase_options.dart';
+
+class Loginview extends StatefulWidget {
+  const Loginview({Key? key}) : super(key: key);
+
+  @override
+  State<Loginview> createState() => _LoginviewState();
+}
+
+class _LoginviewState extends State<Loginview> {
+ late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+                  options: DefaultFirebaseOptions.currentPlatform,
+                ),
+        builder: (context, snapshot) {
+        switch(snapshot.connectionState) {
+
+            case ConnectionState.done:
+          return Column(
+          children: [
+            TextField(
+              controller: _email,
+               enableSuggestions: false,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: 'Enter your email'
+              ),
+            ),
+            TextField(
+              controller: _password,
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration: InputDecoration(
+                hintText: 'Enter your password'
+              ),
+            ),
+            TextButton(
+              onPressed: () async{
+                final email = _email.text;
+                final password = _password.text;
+                try {
+                  final UserCredential =
+                await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+                print('UserCredential');
+                } on FirebaseAuthException
+                 catch (e) {
+                if (e.code == 'user-not-found') {
+                  print('user not found');
+                }else if(e.code == 'wrong-password') {
+                  print('wrong password');
+                }
+                }
+                
+              },
+            child: const Text('Login'),),
+          ],
+        );   
+        default:
+        return const Text('Loading...');
+        }
+          
+        },
+      ),
+    );
+  }
+  
+}
